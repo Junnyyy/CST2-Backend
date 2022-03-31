65f6ac86f0d8b9cb0ca9d61b9c95ccf0b5075842
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-03-30 22:32:01 -0500

    added frontend render

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// View engine setup
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', './views');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -17,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/public', express.static('public'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -24,7 +30,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+/*app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -32,6 +38,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+});*/
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../public/html', 'index.html'));
+  //res.sendFile(path.join(__dirname, '../public/html', 'index.html'));
   //res.render('index', { title: 'Hello World' });
+  res.render('index.ejs', { title: 'Houston Museum of Fine Arts' });
 });
 
 module.exports = router;
