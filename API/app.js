b5cbe25746de440a068c3453a914cbf70595c3fa
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-03-31 02:26:16 -0500

    Test API setup

--- API/app.js
+++ API/app.js
@@ -4,8 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testAPIRouter = require('./routes/testAPI');
 
 var app = express();
 
@@ -22,12 +24,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/testAPI', testAPIRouter);
 app.use('/public', express.static('public'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   next(createError(404));
-});
+});*/
 
 // error handler
 /*app.use(function(err, req, res, next) {
