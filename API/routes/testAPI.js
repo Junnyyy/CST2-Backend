24d9dda833dbe983ca7d3ff927f06ad95352f121
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-03-31 19:17:53 -0500

    Test API completion

--- API/routes/testAPI.js
+++ API/routes/testAPI.js
@@ -3,13 +3,21 @@ var router = express.Router();
 router.use(express.json());
 //var database = require('./database.js');
 
-router.get("/", function (req, res, next) {
-  res.json("API is working properly");
+emails = [ // Example email data set
+  { email: "bobbie@gmail.com" },
+  { email: "josh@gmail.com" },
+  { email: "data@github.com" },
+];
+
+router.get("/", function (req, res, next) { 
+  res.json(emails); // Returns json emails on GET request
 });
 
 router.post("/", function (req, res, next) {
-  const { email } = req.body;
-  console.log(email);
-  res.json(email);
+  const { email } = req.body; // Save request data
+  var emailTemp = { email: email }; // Create temporary variable
+  emails.push(emailTemp) // Add to dataset
+
+  res.sendStatus(200) // all good status
 });
 module.exports = router;
