135f3ee1eee3996eac0e982506d5ff4dc93c7c1f
John <44646589+Junnyyy@users.noreply.github.com>
2022-04-05 03:49:26 -0500

    username conflict x2
    on windows username is system name

--- helpers/database.js
+++ helpers/database.js
@@ -3,12 +3,10 @@ const fs = require('fs');
 //const mysql = require("mysql");
 const mysql = require("mysql2");
 
-console.log(process.env);
-
 // Database connection setup
 var connection = mysql.createConnection({
   host: process.env.HOST,
-  user: process.env.USERNAME,
+  user: process.env.SQLUSER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
   ssl: { 
