6dbe31b44b2cec93f362f5e9b7a04ae3c7d1c46d
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-04-13 16:59:22 -0500

    Completed authentication

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,10 @@ router.use(express.json());
 const bcrypt = require("bcrypt");
 var database = require("../helpers/database.js");
 
-// Encryption
-//const salt = await bcrypt.genSalt(10);
-
 router.post("/register", async (req, res, next) => {
   // Password encryption
   const salt = await bcrypt.genSalt(10);
   var hashedPassword = await bcrypt.hash(req.body.password, salt);
-  //res.json([hashedPassword, hashedPassword2, validPass, validPass2]);
 
   const user = {
     firstname: req.body.firstname,
@@ -41,9 +37,31 @@ router.post("/register", async (req, res, next) => {
       throw err;
     }
     res.json(user.username);
-    //res.sendStatus(200);
-  }); //
+  });
 });
-//const validPass = await bcrypt.compare(req.body.password, hashedPassword2);
+
+router.post("/login", async (req, res, next) => {
+  var query =
+    "SELECT Employee_password FROM EMPLOYEE WHERE Employee_username = ?;";
+  database.query(query, [req.body.username], async (err, result) => {
+    if (err) {
+      res.sendStatus(500);
+      throw err;
+    }
+
+    // Status 400 if no username found
+    if (result.length === 0)
+      return res.status(400).json({ status: "incorrect" });
+
+    // Compare password
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      result[0].Employee_password
+    );
+    if (!validPassword) return res.status(400).json({ status: "incorrect" });
+
+    res.status(200).json({ status: "correct" });
+  });
+});
 
 module.exports = router;
