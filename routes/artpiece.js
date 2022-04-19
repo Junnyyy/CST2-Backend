fd21756fb67bfcb3199ca539bb4fc9b95847a7ad
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-04-19 16:51:14 -0500

    Update artpiece.js

--- routes/artpiece.js
+++ routes/artpiece.js
@@ -4,8 +4,9 @@ router.use(express.json());
 var database = require("../helpers/database.js");
 
 router.get("/", function (req, res, next) {
-  const query ='SELECT Art_Piece_Title, Date_Created, Medium, Creator_F_Name, Creator_L_Name, Being_Refurbished, Culture, Piece_Height, Piece_Length, Piece_Width, Gallery_Loc, Exhibit_ID FROM ART_PIECE;';
-  database.query(query,function (err, result) {
+  const query =
+    "SELECT Art_Piece_Title, Date_Created, Medium, Creator_F_Name, Creator_L_Name, Being_Refurbished, Culture, Piece_Height, Piece_Length, Piece_Width, Gallery_Loc, Exhibit_ID FROM ART_PIECE;";
+  database.query(query, function (err, result) {
     if (err) {
       res.sendStatus(500);
       throw err;
@@ -19,8 +20,21 @@ router.post("/", function (req, res, next) {
   if (Object.keys(req.body).length < 3) return res.status(400);
 
   const newArt = req.body;
-  var data = [newArt.title, newArt.created, newArt.medium, newArt.firstname, newArt.lastname, newArt.status, newArt.culture, newArt.height, newArt.len, newArt.width, newArt.galLoc, newArt.EID];
-
+  var data = [
+    newArt.title,
+    newArt.created,
+    newArt.medium,
+    newArt.firstname,
+    newArt.lastname,
+    newArt.status,
+    newArt.culture,
+    newArt.height,
+    newArt.len,
+    newArt.width,
+    newArt.galLoc,
+    newArt.EID,
+  ];
+  console.log(data);
   const query =
     "INSERT INTO ART_PIECE(Art_Piece_Title, Date_Created, Medium, Creator_F_Name, Creator_L_Name, Being_Refurbished, Culture, Piece_Height, Piece_Length, Piece_Width, Gallery_Loc, Exhibit_ID) VALUES (?);";
   database.query(query, [data], function (err, result) {
@@ -29,9 +43,10 @@ router.post("/", function (req, res, next) {
       throw err;
     }
   });
-  const returnquery = "SELECT Art_Piece_ID FROM ART_PIECE WHERE Art_Piece_Title=?;";
-  database.query(returnquery, newArt.title, function(err,result){
-    if(err) {
+  const returnquery =
+    "SELECT Art_Piece_ID FROM ART_PIECE WHERE Art_Piece_Title=?;";
+  database.query(returnquery, newArt.title, function (err, result) {
+    if (err) {
       res.sendStatus(500);
       throw err;
     }
@@ -39,15 +54,15 @@ router.post("/", function (req, res, next) {
   });
 });
 
-router.delete("/", function(req, res, next) {
+router.delete("/", function (req, res, next) {
   if (Object.keys(req.body).length < 1) return res.status(400);
 
   const delArt = req.body;
   var data = [delArt.ID];
 
   const query = "DELETE FROM ART_PIECE WHERE Art_Piece_ID =?;";
-  database.query(query, [data], function(err,result) {
-    if(err) {
+  database.query(query, [data], function (err, result) {
+    if (err) {
       res.sendStatus(500);
       throw err;
     }
