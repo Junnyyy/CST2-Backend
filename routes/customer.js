268da545833d354ca7005b4749164c0d9e10e01f
alyssaguillory <98233455+alyssaguillory@users.noreply.github.com>
2022-04-19 14:13:09 -0500

    PUT request

--- routes/customer.js
+++ routes/customer.js
@@ -12,45 +12,6 @@ router.get("/", function (req, res, next) {
     res.json(result)
   });
 });
-//FIX CODE BELOW!!!!!!
-/*router.patch("/", function(req, res, next) {
-  const updateEx = req.body;
-  // use primary key to find row to modify
-  const Squery = "SELECT Exhibit_Name, Arrival_Date, Departure_Date, Permanent, Ticket_Price, Number_Tickets_Sold, Managing_Department FROM EXHIBIT WHERE Exhibit_ID=?;";
-  database.query(Squery,updateEx.EID,function(err,results){
-    if(err) {
-      //row doesn't exist
-      res.sendStatus(404);
-      throw err;
-    }
-    // creates an array that holds the key values that the query returned
-    var rs = Object.getOwnPropertyNames(results);
-    var newData = [];
-    // primary key cannot be modified
-    var galPK = delGal.name;
-    //if an attribute is not to be modified, then the original req will have that key assigned to a value that is an empty string
-    //if an attribute is to be modified, then the original req will hold that value in the associated key
-    if(delGal.manager=="") {
-      var newManager =rs[1];
-    }
-    else {
-      var newManager = delGal.manager;
-    }
-    if(delGal.capacity=="") {
-      var newCapacity = rs[2];
-    }
-    else {
-      var newCapacity = delGal.capacity;
-    }
-    const Uquery = "UPDATE GALLERY SET Managing_Department=?, Capacity=? WHERE Gallery_Name=?;";
-    database.query(Uquery,[newManager, newCapacity, galPK], function(err,result){
-      if(err) {
-        throw err;
-      }
-    });
-  });
-  res.sendStatus(200);
-});*/
 router.post("/", function (req, res, next) {
   // Data validation
   if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
