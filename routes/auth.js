b5d4b2d69a570fd7f5fadebdcf989756e2a8824e
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-04-19 13:56:23 -0500

    admin flag addition

--- routes/auth.js
+++ routes/auth.js
@@ -49,7 +49,7 @@ router.post("/login", async (req, res, next) => {
   if (Object.keys(req.body).length < 2) return res.status(400);
 
   var query =
-    "SELECT Employee_password, Employee_ID, Employee_F_Name, Employee_Email FROM EMPLOYEE WHERE Employee_username = ?;";
+    "SELECT Employee_password, Employee_ID, Employee_F_Name, Employee_Email, Admin_Flag FROM EMPLOYEE WHERE Employee_username = ?;";
   database.query(query, [req.body.username], async (err, result) => {
     if (err) {
       res.sendStatus(500);
@@ -72,12 +72,15 @@ router.post("/login", async (req, res, next) => {
       return res.status(401).json({ error: "not found" });
     }
 
+    var flag = false;
+    if (result[0].Admin_Flag == 1) flag = true;
+
     const token = generateAccessToken({
       username: req.body.username,
       id: result[0].Employee_ID,
       firstname: result[0].Employee_F_Name,
       email: result[0].Employee_Email,
-      admin: false,
+      admin: flag,
     });
     res.status(200).json({ token: token });
   });
